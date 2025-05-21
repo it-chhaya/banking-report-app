1a7ee1586519b5b703ebe37c79506e44182ce496
techmentors2024 <techmentors2024@gmail.com>
2025-05-21 19:04:01 +0700

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,71 @@
+import type { Metadata, Viewport } from "next";
+import { fontVariables } from "@/lib/font";
+import "./globals.css";
+import "./theme.css"
+import { ThemeProvider } from "@/components/layout/ThemeToggle/theme-provider";
+import { cn } from "@/lib/utils";
+import { cookies } from "next/headers";
+import { ActiveThemeProvider } from "@/components/active-theme";
+
+const META_THEME_COLORS = {
+  light: '#ffffff',
+  dark: '#09090b'
+};
+
+export const metadata: Metadata = {
+  title: 'Next Shadcn',
+  description: 'Basic dashboard with Next.js and Shadcn'
+};
+
+export const viewport: Viewport = {
+  themeColor: META_THEME_COLORS.light
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  const cookieStore = await cookies();
+  const activeThemeValue = cookieStore.get('active_theme')?.value;
+  const isScaled = activeThemeValue?.endsWith('-scaled');
+
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              try {
+                if (localStorage.theme === 'dark' || ((!('theme' in localStorage) || localStorage.theme === 'system') && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                  document.querySelector('meta[name="theme-color"]').setAttribute('content', '${META_THEME_COLORS.dark}')
+                }
+              } catch (_) {}
+            `
+          }}
+        />
+      </head>
+      <body
+        className={cn(
+          'bg-background font-sans antialiased',
+          activeThemeValue ? `theme-${activeThemeValue}` : '',
+          isScaled ? 'theme-scaled' : '',
+          fontVariables
+        )}
+      >
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            enableColorScheme
+          >
+            <ActiveThemeProvider initialTheme={activeThemeValue}>
+              {children}
+            </ActiveThemeProvider>
+          </ThemeProvider>
+      </body>
+    </html>
+  );
+}
