ac3e3ce80743a4a839c299ea7332547293dd469a
techmentors2024 <techmentors2024@gmail.com>
2025-05-28 10:19:39 +0700

    add react-hook-form and zod

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -6,23 +6,58 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import Link from "next/link"
 import Image from "next/image"
-import { useRouter } from "next/navigation"
+// import { useRouter } from "next/navigation"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 chars"
+  })
+    .max(50, {
+      message: "Username must be 50 max chars"
+    }),
+  password: z.string().nonempty({
+    message: "Password is required"
+  })
+})
+
+// type LoginRequest = {
+//   username: string;
+//   password: string;
+// }
+
+
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"div">) {
 
-  const router = useRouter()
+  //const router = useRouter()
 
-  function login() {
-    // login logic
-    router.push("/dashboard")
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema)
+  })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch
+  } = form
+
+  const username = watch("username")
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    console.log("Data", data)
   }
 
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="flex flex-col gap-6">
           <div className="flex flex-col items-center gap-2">
             <Link
@@ -34,30 +69,34 @@ export function LoginForm({
               </div>
               <span className="sr-only">iSTAD</span>
             </Link>
-            <h1 className="text-xl font-bold">Welcome to iSTAD</h1>
+            <h1 className="text-xl font-bold">Welcome {username} to iSTAD</h1>
           </div>
           <div className="flex flex-col gap-6">
             <div className="grid gap-2">
               <Label htmlFor="username">Username</Label>
               <Input
+                {...register("username")}
                 id="username"
                 type="text"
                 placeholder="username"
-                required
+                className={errors.username && "border-red-500 focus-visible:outline-red-500 focus-visible:border-2 focus-visible:border-red-500"}
               />
+              <p className="text-red-500">{errors.username?.message}</p>
             </div>
             <div className="grid gap-2">
               <Label htmlFor="password">Password</Label>
               <Input
+                {...register("password")}
                 id="password"
                 type="password"
                 placeholder="password"
-                required
               />
+              <p className="text-red-500">
+                {errors.password?.message}
+              </p>
             </div>
             <Button
-              onClick={login}
-              type="button" className="w-full">
+              type="submit" className="w-full">
               Login
             </Button>
           </div>
