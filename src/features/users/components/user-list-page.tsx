035eefb5650d0f939f4688df20949bf725a27e92
techmentors2024 <techmentors2024@gmail.com>
2025-06-02 18:28:22 +0700

    basic data-table with customized cell

--- src/features/users/components/user-list-page.tsx
+++ src/features/users/components/user-list-page.tsx
@@ -1,40 +1,12 @@
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table"
-import { invoices } from "../user-mock"
+import { payments } from "../user-mock";
+import { columns } from "./columns";
+import { DataTable } from "./data-table";
 
 export default function UserListPage() {
     return (
-        <Table>
-            <TableCaption>A list of your recent invoices.</TableCaption>
-            <TableHeader>
-                <TableRow>
-                    <TableHead className="w-[100px]">Invoice</TableHead>
-                    <TableHead>Status</TableHead>
-                    <TableHead>Method</TableHead>
-                    <TableHead className="text-right">Amount</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {
-                    invoices.map(invoice => {
-                        return (
-                            <TableRow key={invoice.invoice}>
-                                <TableCell className="font-medium">{invoice.invoice}</TableCell>
-                                <TableCell>{invoice.paymentStatus}</TableCell>
-                                <TableCell>{invoice.paymentMethod}</TableCell>
-                                <TableCell className="text-right">{invoice.totalAmount}</TableCell>
-                            </TableRow>
-                        )
-                    })
-                }
-            </TableBody>
-        </Table>
+        <DataTable
+            columns={columns}
+            data={payments}
+        />
     )
 }
